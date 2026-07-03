acb058341e58af2b58c56d2c0050dce3864ce81a
Zeinebou20 <zeynebouelmehdi@gmail.com>
2026-07-03 20:12:34 +0100

    resol error

--- src/symbolic_engine.py
+++ src/symbolic_engine.py
@@ -1,12 +1,9 @@
-# type: ignore[import-untyped]
-from sympy import Expr, Symbol, sin, diff
+from sympy import Expr, Symbol, sin, diff  # type: ignore[import-untyped]
 
 def get_derivative_of_sin_x2(x: Symbol) -> Expr:
-    # نقوم بإجراء الحساب مباشرة
     return diff(sin(x**2), x)
 
 if __name__ == "__main__":
-    # تعريف الرمز
     x_sym: Symbol = Symbol('x')
     derivative: Expr = get_derivative_of_sin_x2(x_sym)
     print(f"المشتقة هي: {derivative}")
