0da70e356d693eb77c681772995b2b6cbef07dfd
Zeinebou20 <zeynebouelmehdi@gmail.com>
2026-07-03 19:25:06 +0100

    resolve error

--- src/symbolic_engine.py
+++ src/symbolic_engine.py
@@ -1,15 +1,15 @@
 import sympy as sp
-# type: ignore[import-untyped]
+from sympy import Expr, Symbol, sin, diff
 
-def get_derivative_of_sin_x2(x: Expr) -> Expr:
-
-    x = sp.symbols('x')
-    f = sp.sin(x**2)
-    
-    df = sp.diff(f, x)
-    
+def get_derivative_of_sin_x2(x: Symbol) -> Expr:
+   
+    f: Expr = sin(x**2)
+    df: Expr = diff(f, x)
     return df
 
 if __name__ == "__main__":
-    derivative = get_derivative_of_sin_x2()
-    print(f" {derivative}")
+    x_sym: Symbol = Symbol('x')
+    
+    derivative: Expr = get_derivative_of_sin_x2(x_sym)
+    
+    print(f"المشتقة هي: {derivative}")
